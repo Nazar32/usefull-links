5f69eb16ed39e20229b2da33e7ef289a22df7257
Nazar32 <nazar3223@gmail.com>
2020-04-30 21:34:43 +0300

    Updates

---
+++ precache-manifest.29db66934ae158052406feb389397407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0228bad2763a798067939449191210",
+    "url": "/usefull-links/index.html"
+  },
+  {
+    "revision": "cbe50424e442bf38a9f2",
+    "url": "/usefull-links/static/css/main.61253683.chunk.css"
+  },
+  {
+    "revision": "d4cd97f128c72117e19a",
+    "url": "/usefull-links/static/js/2.87815282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/usefull-links/static/js/2.87815282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe50424e442bf38a9f2",
+    "url": "/usefull-links/static/js/main.a53bb7f5.chunk.js"
+  },
+  {
+    "revision": "28653c595afdd5a81504",
+    "url": "/usefull-links/static/js/runtime-main.8b95a4cf.js"
+  }
+]);
