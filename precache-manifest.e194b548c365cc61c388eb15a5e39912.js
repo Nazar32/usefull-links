33d9415d62126961eaf17b66875c11ccda1b602a
Nazar32 <Nazar32@users.noreply.github.com>
2020-10-21 09:00:59 +0000

    deploy: 1b7ed91f8a6bbc9965733309b7f4b4ffb51541e2

--- precache-manifest.e194b548c365cc61c388eb15a5e39912.js
+++ precache-manifest.e194b548c365cc61c388eb15a5e39912.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4ad2fd6e0e98c0c5cbbf4cbb135176d",
+    "revision": "5ae6c7258cfa33df5b4743fc8f67b0c1",
     "url": "/usefull-links/index.html"
   },
   {
-    "revision": "43f98d2c8d6a418a77cb",
+    "revision": "7d22ada9f377e0acd02d",
     "url": "/usefull-links/static/css/main.f300e3bf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/usefull-links/static/js/2.c37c90db.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "43f98d2c8d6a418a77cb",
-    "url": "/usefull-links/static/js/main.3ea3d510.chunk.js"
+    "revision": "7d22ada9f377e0acd02d",
+    "url": "/usefull-links/static/js/main.e3826869.chunk.js"
   },
   {
     "revision": "7fd69c6c71bf75e01d02",
